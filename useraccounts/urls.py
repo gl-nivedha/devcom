89fccec7761ec038b77fa7f199cb676731c1ecee
Nivedha <gl.nivedha@gmail.com>
2023-03-02 09:45:57 +0530

    first commit

--- useraccounts/urls.py
+++ useraccounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user/register/',views.register,name='register'),
+    path('user/register',views.register,name='register'),
     path('user/login/',views.login,name='login'),
    
 
--- useraccounts/views.py
+++ useraccounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib import messages
-from django.contrib.auth.models import User,auth
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 def register(request):
@@ -26,7 +26,7 @@ def register(request):
         else:
             print("password not matching")  
             return redirect('user/register/')  
-        return redirect('/')
+        
         
 
 
